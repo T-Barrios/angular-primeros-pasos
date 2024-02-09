e092661b87f11e31faa15bdf6ba32cb55fd0b37f
t-barrios <cherub.nevermind@gmail.com>
2024-02-09 07:40:01 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/components/counter.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    DbzModule,
+    BrowserModule,
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
